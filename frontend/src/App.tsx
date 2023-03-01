aa5651e2e259275043eba422f63c525267d6b45d
ianMuchesia <ianmuchesia@yahoo.com>
2023-03-01 12:44:42 +0300

    starting afresh

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,32 +1,33 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
-import { Home, Create } from "./pages"
-import {Batman_homePage} from "./assets"
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
 
 function App() {
-
+  const [count, setCount] = useState(0)
 
   return (
-    <BrowserRouter>
-    <header className=" absolute z-10 w-full flex justify-between items-center sm:px-8 px-4 py-4 ">
-        <Link to="/">
-          <h1 className="text-xl text-gray-100">Batman</h1>
-        </Link>
-        <Link
-          to="Create"
-          className="font-inter font-medium bg-red-500  text-white px-4 py-2 rounded-md"
-        >
-          create
-        </Link>
-
-        </header>
-    <main  >
-    <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="Create" element={<Create/>}/>
-    </Routes>
-    </main>
-      
-    </BrowserRouter>
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
   )
 }
 
--- frontend/src/assets/index.ts
+++
@@ -1,6 +0,0 @@
-import Batman_homePage from "./Batman_homePage.jpg"
-import Batman_createPage from "./Batman_createPage.jpg"
-
-export {
-    Batman_createPage, Batman_homePage
-}
